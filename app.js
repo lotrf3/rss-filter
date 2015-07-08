7837bf02cd005089ad7f72818914d77348cb6842
lotrf3 <allan.c.rempel@gmail.com>
2015-07-08 03:57:48 -0500

    Added dependencies, error handling, and openshift data

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 function filter(obj, query){
 	if (typeof query == "object") {
 		for(var property in query)
@@ -29,27 +32,35 @@ app.get('/url/:url/:path', function(req, res){
     if (!error && response.statusCode == 200) {
 		var parser = new xml2js.Parser();
 		parser.parseString(body, function(err, result){
-		
-			var path = req.params.path.split('.');
-			var xml = result;
-			for(var i=0; i <path.length; i++){
-				if(xml.hasOwnProperty(path[i]))
-					xml = xml[path[i]];
-				else
-					res.send("ERROR");
-			}
-			console.log(req.query);
-			for(var i=0; i<xml.length; i++){
-				if(filter(xml[i],req.query)){
-					xml.splice(i,1);
-					i--;
+			if(err)
+				res.status(400).send({ error: 'Bad XML/RSS URL' });
+			else{
+				try{
+					var path = req.params.path.split('.');
+					var xml = result;
+					for(var i=0; i <path.length; i++){
+						if(xml.hasOwnProperty(path[i]))
+							xml = xml[path[i]];
+						else
+							res.status(400).send({ error: 'Bad path' });
+					}
+					for(var i=0; i<xml.length; i++){
+						if(filter(xml[i],req.query)){
+							xml.splice(i,1);
+							i--;
+						}
+					}
+					var builder = new xml2js.Builder();
+					res.set('Content-Type', 'text/xml');
+					res.send(builder.buildObject(result));
+				} catch (err){
+					res.status(500).send({ error: 'Oops, server error' });
 				}
 			}
-			var builder = new xml2js.Builder();
-			res.set('Content-Type', 'text/xml');
-			res.send(builder.buildObject(result));
 		});
     }
+	else
+		res.status(500).send({ error: 'Oops, server error' });
   })
 });
-app.listen(3000, function(){});
+app.listen(server_port, server_ip_address, function(){});
