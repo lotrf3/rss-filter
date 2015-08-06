5f67d3959b98fb4aa20d593e474f6b869cc29f16
lotrf3 <allan.c.rempel@gmail.com>
2015-08-05 23:44:43 -0500

    Reformated to use query params rather than path

--- app.js
+++ app.js
@@ -8,29 +8,50 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static('public'));
 
-function filter(obj, query){
-	if (typeof query == "object") {
-		for(var property in query)
-			if (query.hasOwnProperty(property)) {
-				if(query instanceof Array){
-					if(filter(obj, query[property]))
-						return true;
-				} else {
-					if(filter(obj[property], query[property]))
-						return true;
-				}
-			}
-	}else{
-		obj = obj[0];
-		var regexp = new RegExp(query, "i");
-		if(obj.match(regexp))
-			return true;
-	}
-	return false;
+
+function is(obj, query){
+	var term = query[0].split('.');
+	for(var i in term)
+		obj = obj[term[i]];
+	var regexp = new RegExp(query[1], "i");
+	if(obj.match(regexp))
+		return true;
+	else
+		return false;
 }
 
-app.get('/url/:url/:path', function(req, res){
-  request(req.params.url, function (error, response, body) {
+function filter(obj, query){
+
+	if (typeof query == "object") {
+		if (query.is){
+			return is(obj, query.is);
+		}
+		else if (query.isnot){
+			return !is(obj, query.isnot);
+		}
+		else if (query.and){
+			for(var i in query.and){
+				if(!filter(obj, query.and[i]))
+					return false;
+			}
+			return true;
+		}
+		else if (query.or){
+			for(var i in query.or){
+				if(filter(obj, query.or[i]))
+					return true;
+			}
+			return false;
+		}
+		else
+			throw "Invalid query JSON";
+	}else{
+		throw "Bad query";
+	}
+}
+
+app.get('/filter', function(req, res){
+  request(req.query.url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
 		var parser = new xml2js.Parser();
 		parser.parseString(body, function(err, result){
@@ -38,7 +59,7 @@ app.get('/url/:url/:path', function(req, res){
 				res.status(400).send({ error: 'Bad XML/RSS URL' });
 			else{
 				try{
-					var path = req.params.path.split('.');
+					var path = req.query.path.split('.');
 					var xml = result;
 					for(var i=0; i <path.length; i++){
 						if(xml.hasOwnProperty(path[i]))
@@ -47,7 +68,7 @@ app.get('/url/:url/:path', function(req, res){
 							res.status(400).send({ error: 'Bad path' });
 					}
 					for(var i=0; i<xml.length; i++){
-						if(filter(xml[i],req.query)){
+						if(filter(xml[i],JSON.parse(req.query.q))){
 							xml.splice(i,1);
 							i--;
 						}
@@ -56,13 +77,13 @@ app.get('/url/:url/:path', function(req, res){
 					res.set('Content-Type', 'text/xml');
 					res.send(builder.buildObject(result));
 				} catch (err){
-					res.status(500).send({ error: 'Oops, server error' });
+					res.status(500).send({ error: err });
 				}
 			}
 		});
     }
 	else
 		res.status(500).send({ error: 'Oops, server error' });
-  })
+  });
 });
 app.listen(server_port, server_ip_address, function(){});
--- public/builder.js
+++ public/builder.js
@@ -2,7 +2,7 @@ function build() {
 	var form = document.forms["builder"]
     var url = encodeURIComponent(form["url"].value);
 	var path = encodeURIComponent(form["path"].value);
-	var query = form["query"].value;
-	var result = window.location.hostname + "/url/" + url + "/" + path + "?" + query;
+	var query = encodeURIComponent(form["query"].value);
+	var result = window.location.hostname + "/filter?url=" + url + "&path=" + path + "&q=" + query;
 	form["result"].value = result;
 }
